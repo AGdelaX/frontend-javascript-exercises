0f2953c4d7f2dab87a82fca83fc86b7888bcb20c
Alberto de la Cruz <agdelax@gmail.com>
2015-05-08 15:03:13 -0400

    removed square brackets in function argument

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -2,13 +2,13 @@ module.exports.newArray = function(a, b, c, d){
 	return [a, b, c, d];
 };
 
-module.exports.firstAndLast = function([input]){
+module.exports.firstAndLast = function(input){
 	var array = [input];
 	var lastElementIndex= array.length-1;
 
 	var a = array[0];
 	var b = array[lastElementIndex];
 	var newArray = [a, b];
-	
+
 	return newArray;
 };
