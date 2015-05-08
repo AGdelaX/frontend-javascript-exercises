8adaebd942f61bd3b9aea0b29638273293aa88fd
Alberto de la Cruz <agdelax@gmail.com>
2015-05-08 14:53:01 -0400

    Arguments array without brackets

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -2,7 +2,7 @@ module.exports.newArray = function(a, b, c, d){
 	return [a, b, c, d];
 };
 
-module.exports.firstAndLast = function([]){
+module.exports.firstAndLast = function(){
 	var a = arguments[0];
 	var lastIndex = arguments.length-1;
 	var b = arguments[lastIndex];
