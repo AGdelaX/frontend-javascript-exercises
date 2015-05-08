4d31186a4cbf93a174867243535a299666f247ec
Alberto de la Cruz <agdelax@gmail.com>
2015-05-08 14:52:05 -0400

    Experimenting with the arguments object

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -2,20 +2,12 @@ module.exports.newArray = function(a, b, c, d){
 	return [a, b, c, d];
 };
 
-module.exports.firstAndLast = function(a, b, c, d){
-	var argumentsArray = [a,b,c,d];
-	var array = [1, 2];
-	array[0]= a;
-
-	var end= argumentsArray.length-1;
-
-	if (argumentsArray[end]===undefined){
-		argumentsArray.pop;
-		end = argumentsArray.length-1;
-		array[1]=argumentsArray[end];
-	} 
-	else {
-		array[1]=argumentsArray[end];
-	}
+module.exports.firstAndLast = function([]){
+	var a = arguments[0];
+	var lastIndex = arguments.length-1;
+	var b = arguments[lastIndex];
+	var array = [];
+	array.push(b);
+	array.unshift(a);
 	return array;
 };
