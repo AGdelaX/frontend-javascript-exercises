f54f0f0766c720402fb3ce186044f9c952ed4059
Alberto de la Cruz <agdelax@gmail.com>
2015-05-08 12:30:41 -0400

    testing an if/else statement in firstAndLast

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -2,7 +2,16 @@ module.exports.newArray = function(a, b, c, d){
 	return [a, b, c, d];
 };
 
-module.exports.firstAndLast = function(input){
-	var array = [input];
-	return array[0, array.length-1];
+module.exports.firstAndLast = function(a, b, c, d){
+	var array = [1, 2];
+	array[1]= a;
+	
+	if (array[array.length-1]===undefined){
+		array.pop;
+		array[2]=array.length-1;
+	} 
+	else {
+		array[2]=array.length-1;
+	}
+	return array;
 };
