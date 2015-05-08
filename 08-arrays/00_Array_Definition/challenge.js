4fb4e4ffc946b6441d6dba554b1b6cbb6ec001de
Alberto de la Cruz <agdelax@gmail.com>
2015-05-08 16:30:55 -0400

    testing array prototype

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -4,7 +4,7 @@ module.exports.newArray = function(a, b, c, d){
 
 
 module.exports.firstAndLast = function(array){
-	var arg = Array.from(arguments);
+	var arg = Array.prototyp.slice.call(arguments);
 
 	var end = arg.length-1;
 
