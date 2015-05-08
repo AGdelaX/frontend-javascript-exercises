00c1cbc06f47447eefd0ec84e46778425bb74ba4
Alberto de la Cruz <agdelax@gmail.com>
2015-05-07 22:09:39 -0400

    Made new function for outsideRanges and fixed typo in nameAndPrice

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -11,10 +11,10 @@ module.exports.inBetween = function(lower, middle, upper) {
 };
 
 module.exports.outsideRanges = function(number) {
-	return (number < 7) && (number <= 42) && (10>number&&number>20);
+	return ((10>number)||(number>20))&&((number<=42)||(number<75))&&((number<=1)||(number>=6));
 
 };
 
 module.exports.nameAndPrice = function(name, price) {
-	return (name==="NYTimes" && price <=1) || (name === 'LATimes' && price>=1);
+	return (name==="NYTimes" && price >=1) || (name === 'LATimes' && price>=1);
 };
