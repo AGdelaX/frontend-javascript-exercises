9bd85745e90157434b5ed95eeae57300b2204364
Alberto de la Cruz <agdelax@gmail.com>
2015-05-07 21:37:02 -0400

    Wrote Boolean functions

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,19 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+	return stringOne == stringTwo;
 };
 
 module.exports.notEqual = function(one, two) {
-
+	return one !== two;
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+	return lower < middle&&middle < upper;
 };
 
 module.exports.outsideRanges = function(number) {
-
+	return (number < 7) || (number <= 42) || (10>number&&number>20);
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+	return (name==="NYTimes" && price <=1) || (name === 'LATimes' && price>=1);
 };
