d6c81d36fca48771dc3a075e987b6c7b3b99b88c
Alberto de la Cruz <agdelax@gmail.com>
2015-05-08 12:36:25 -0400

    edited if/else in firstAndLast

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -3,12 +3,15 @@ module.exports.newArray = function(a, b, c, d){
 };
 
 module.exports.firstAndLast = function(a, b, c, d){
+	var argumentsArray = [a,b,c,d];
 	var array = [1, 2];
-	array[1]= a;
-	
-	if (array[array.length-1]===undefined){
-		array.pop;
-		array[2]=array.length-1;
+	array[0]= a;
+	var end=argumentsArray.length-1;
+
+	if (argumentsArray[end]===undefined){
+		argumentsArray.pop;
+		end= argumentsArray.length-1;
+		array[1]=argumentsArray[end];
 	} 
 	else {
 		array[2]=array.length-1;
