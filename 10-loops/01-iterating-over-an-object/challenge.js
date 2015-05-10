657056f0e61b457ea9d470be761a317d65314f95
Alberto de la Cruz <agdelax@gmail.com>
2015-05-10 15:06:51 -0400

    Completed second set of loop exercises

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(object){
+	var emptyArray = [];
+	for (var property in object) {
+		emptyArray.push(property);
+	}
+	return emptyArray;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(object){
+	var emptyArray = [];
+	for (var property in object) {
+		emptyArray.push(object[property]);
+	}
+	return emptyArray;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(object){
+	var emptyArray = [];
+	for (var property in object) {
+		emptyArray.push(property+" is "+object[property]);
+	}
+	return emptyArray;
+};
