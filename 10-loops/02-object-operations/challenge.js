7a7f728437a64802350f90599c586d01c8656c5b
Alberto de la Cruz <agdelax@gmail.com>
2015-05-10 15:54:05 -0400

    completed second set of loop exercises

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,26 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object){
+	var newObject = {};
+	for (var prop in object){
+		newObject[prop]=object[prop];
+	}
+	return newObject;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src){
+	for (var prop in src){
+		dest[prop]=src[prop];
+	}
+	return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(object, array){
+	var emptyArray= [];
+	for (var i=0; i<array.length; i++){
+		object[array[i]]=array[i];
+	}
+	for (var prop in object) {
+		emptyArray.push(object[prop]);
+	}
+	array.toString === emptyArray.toString;
+
+};
--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,19 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionFn, actionFn){
+	var i = 0;
+	while (i < 11){
+		conditionFn == true;
+		actionFn;
+		i ++;
+	}
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+	var sum = 0;
+	var i = 0;
+	while (i<array.length) {
+		sum += array[i];
+		i++;
+	}
+	return sum;
+};
 
