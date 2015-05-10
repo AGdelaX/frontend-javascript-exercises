a0dacbc8cbfa67aeb8db682b639e60868c54addb
Alberto de la Cruz <agdelax@gmail.com>
2015-05-10 10:52:54 -0400

    completed objects exercises

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,31 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents){
+	return {
+		title : courseTitle,
+		duration: courseDuration,
+		students: courseStudents
+	};
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue){
+	if (newProp in object){
+		return object;
+	} 
+	else {
+		object[newProp]=newValue;
+		return object;
+	}
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+	return "Hello "+letter.recipient+",\n\n"+letter.msg+"\n\nSincerely,\n"+letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money){
+	var inventory = {
+		items : ["MacBook Air", "MacBook Pro", "Mac Pro", "Apple Sticker"],
+		prices : [999, 1299, 2499, 1]
+	};
+	var i = inventory.items.indexOf(item);
+	return inventory.prices[i] <= money;
+
+};
