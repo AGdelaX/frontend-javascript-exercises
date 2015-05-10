b5b20dd789c849c4f2a96c33de908555fa6cf8d0
Alberto de la Cruz <agdelax@gmail.com>
2015-05-10 12:22:12 -0400

    completed first exercises for for loops

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array){
+	var result =0;
+	for (var counter=0; counter<array.length; counter++){
+		result += array[counter];
+	};
+	return result;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(string){
+	var split = string.toLowerCase().split(",");
+	return split;
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array){
+	var emptyArray = [];
+	for (var i = 0; i<array.length; i++){
+		emptyArray.push(i+" is "+array[i]);
+	}
+	return emptyArray;
+};
